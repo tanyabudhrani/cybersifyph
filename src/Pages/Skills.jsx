591848736388c465a3f951a5ee749e676480d390
Tanya Budhrani <tanyabudhrani04@gmail.com>
2024-09-19 22:03:46 +0800

    Update Skills.jsx

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -18,6 +18,12 @@ const Skills = () => {
               <SkillsCards />
             </MarqueeCards>
           </div>
+          <div className="relative">
+            <div className="absolute top-[45px] w-full h-[1px] bg-primary-400"></div>
+            <MarqueeCards direction="left">
+              <SkillsCards />
+            </MarqueeCards>
+          </div>
         </div>
       </div>
     </div>
