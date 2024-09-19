0a57e76db7f8635cba54d68cd8a83618798e1182
Tanya Budhrani <tanyabudhrani04@gmail.com>
2024-09-19 22:06:33 +0800

    Update Skills.jsx

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -13,8 +13,14 @@ const Skills = () => {
         </div>
         <div className="xl:border-l-2 xl:border-r-2 xl:border-primary-400 h-full">
           <div className="relative">
-            <div className="absolute top-[45px] left-[10px] right-[10px] h-[1px] bg-primary-400"></div>
+            <div className="absolute top-[45px] w-full h-[1px] bg-primary-400"></div>
             <MarqueeCards direction="left">
+              <SkillsCards />
+            </MarqueeCards>
+          </div>
+          <div className="relative">
+            <div className="absolute top-[45px] w-full h-[1px] bg-primary-400"></div>
+            <MarqueeCards direction="right">
               <SkillsCards />
             </MarqueeCards>
           </div>
@@ -23,6 +29,5 @@ const Skills = () => {
     </div>
   );
 };
-
 
 export default Skills;
