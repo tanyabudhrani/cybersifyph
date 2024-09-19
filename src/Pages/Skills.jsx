1bc16b731320da4b197a0c82b274e125f717d00c
Tanya Budhrani <tanyabudhrani04@gmail.com>
2024-09-19 16:39:55 +0800

    try

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -1,11 +1,4 @@
-/*
-   Copyright (C), 2023-2024, Sara Echeverria (bl33h)
-   Author: Sara Echeverria
-   FileName: Skills.jsx
-   Version: I
-   Creation: 02/06/2023
-   Last modification: 03/06/2023
-*/
+
 
 import MarqueeCards from "../Components/MarqueeCards";
 import SectionTitle from "../Components/SectionTitle";
@@ -16,7 +9,7 @@ const Skills = () => {
     <div id="skills" className="w-full overflow-hidden-web flex justify-center">
       <div className="w-full min-h-[800px] flex flex-col xl:w-[70%]">
         <div className="w-full">
-          <SectionTitle title="SKILLS" subtitle="WHAT I DO" />
+          <SectionTitle title="Partnerships" />
         </div>
         <div className="xl:border-l-2 xl:border-r-2 xl:border-primary-400 h-full">
           <div className="relative">
